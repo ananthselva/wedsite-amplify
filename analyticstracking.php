2bf17b76c42e057d402edda6b7a3405612b48db1
Ananth S <36985986+ananthselva@users.noreply.github.com>
2024-01-12 16:15:01 +0530

    website

---
+++ analyticstracking.php
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NZ9HQ2HPHT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NZ9HQ2HPHT');
+</script>
